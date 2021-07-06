14397686fe96568a109d496585e76e1bc61ffa04
LeonColt <lionmanbloodsucker@yahoo.com>
2021-07-07 05:41:32 +0700

    add lat lng model

---
+++ lib/model/latlng.dart
@@ -0,0 +1,22 @@
+class LatLng {
+  final double lat;
+  final double lng;
+  LatLng({ required this.lat, required this.lng });
+  factory LatLng.fromJson( final Map<String, dynamic> json ) {
+    ArgumentError.checkNotNull( json, "LatLng json map" );
+    if ( json.containsKey("type") && json["type"] == "Point" ) {
+      if ( json.containsKey("coordinates") && json["coordinates"] is List ) {
+        return new LatLng(
+            lat: json["coordinates"][1] is String ? num.parse( json["coordinates"][1] ).toDouble() : ( json["coordinates"][1] as num ).toDouble(),
+            lng: json["coordinates"][0] is String ? num.parse( json["coordinates"][0] ).toDouble() : ( json["coordinates"][0] as num ).toDouble() );
+      } else throw new ArgumentError( "Type point missing field coordinates or is not array!" );
+    } else throw new ArgumentError( "GeoJson is not point!" );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "type": "Point",
+      "coordinates": [ lng, lat, ],
+    };
+  }
+}
