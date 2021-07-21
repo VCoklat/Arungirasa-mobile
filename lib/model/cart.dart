7c0708a67ce431dac2c070b0c77962cd356859f2
LeonColt <34907762+LeonColt@users.noreply.github.com>
2021-07-21 21:13:24 +0700

    add factory from json dart fmt

--- lib/model/cart.dart
+++ lib/model/cart.dart
@@ -3,16 +3,24 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'cart.g.dart';
 
-@JsonSerializable( createToJson: false )
+@JsonSerializable(createToJson: false)
 class Cart {
-  @JsonKey( required: true, disallowNullValue: true )
+  @JsonKey(required: true, disallowNullValue: true)
   final String uuid;
 
-  @JsonKey( name: "menuId", required: true, disallowNullValue: true, fromJson: FoodDrinkMenuRef.fromJson, toJson: FoodDrinkMenuRef.toJson )
+  @JsonKey(
+      name: "menuId",
+      required: true,
+      disallowNullValue: true,
+      fromJson: FoodDrinkMenuRef.fromJson,
+      toJson: FoodDrinkMenuRef.toJson)
   final FoodDrinkMenuRef menuRef;
 
-  @JsonKey( required: true, disallowNullValue: true )
+  @JsonKey(required: true, disallowNullValue: true)
   final int qty;
 
   Cart(this.uuid, this.menuRef, this.qty);
+
+  factory Cart.fromJson(final Map<String, dynamic> json) =>
+      _$CartFromJson(json);
 }
