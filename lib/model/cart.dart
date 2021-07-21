e4014e2e262a201a2d041d053a5974281262f189
LeonColt <lionmanbloodsucker@yahoo.com>
2021-07-21 11:32:43 +0700

    add cart model

---
+++ lib/model/cart.dart
@@ -0,0 +1,18 @@
+import 'package:arungi_rasa/model/food_drink_menu.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'cart.g.dart';
+
+@JsonSerializable( createToJson: false )
+class Cart {
+  @JsonKey( required: true, disallowNullValue: true )
+  final String uuid;
+
+  @JsonKey( name: "menuId", required: true, disallowNullValue: true, fromJson: FoodDrinkMenuRef.fromJson, toJson: FoodDrinkMenuRef.toJson )
+  final FoodDrinkMenuRef menuRef;
+
+  @JsonKey( required: true, disallowNullValue: true )
+  final int qty;
+
+  Cart(this.uuid, this.menuRef, this.qty);
+}
