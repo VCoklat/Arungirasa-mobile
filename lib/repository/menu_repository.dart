906177eb1b268ec192adcb217b4702b0b4a30591
LeonColt <lionmanbloodsucker@yahoo.com>
2021-07-21 11:31:49 +0700

    add find one

--- lib/repository/menu_repository.dart
+++ lib/repository/menu_repository.dart
@@ -1,5 +1,6 @@
 import 'package:arungi_rasa/common/config.dart';
 import 'package:arungi_rasa/model/food_drink_menu.dart';
+import 'package:arungi_rasa/model/restaurant.dart';
 import 'package:get/get.dart';
 import 'package:get_connect_repo_mixin/get_connect_repo_mixin.dart';
 
@@ -10,6 +11,13 @@ class FoodDrinkMenuRepository extends GetConnect with RepositorySslHandlerMixin,
     final response = await get( "$kRestUrl/restaurant/$restaurantId/menu${ query == null ? "" : "?query=${Uri.encodeComponent( query )}" }" );
     if (  response.isOk )
       return ( response.body as List ).map( (e) => FoodDrinkMenu.fromJson( e as Map<String, dynamic> ) ).toList( growable: false );
+    else throw getException( response );
+  }
+
+  Future<FoodDrinkMenu> findOne( final RestaurantRef restaurantId, final FoodDrinkMenuRef ref ) async {
+    final response = await get( "$kRestUrl/restaurant/$restaurantId/menu/${ ref.id }" );
+    if (  response.isOk )
+      return FoodDrinkMenu.fromJson( response.body as Map<String, dynamic> );
     else throw getException( response );
   }
 
