22d98342df8e6c461f2f662d07303a4af54274a4
Leon Colt <lionmanbloodsucker@yahoo.com>
2021-08-04 19:43:09 +0700

    add env

---
+++ lib/env/env.dart
@@ -0,0 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'env.g.dart';
+
+// ignore: invalid_annotation_target
+@JsonLiteral("env.json", asConst: true)
+final Map<String, dynamic> _env = _$_envJsonLiteral;
+
+@JsonSerializable(createToJson: false)
+class Env {
+  @JsonKey(required: true, disallowNullValue: true)
+  final String mapBoxAccessToken;
+  Env(this.mapBoxAccessToken);
+  factory Env.fromJson(final Map<String, dynamic> json) => _$EnvFromJson(json);
+}
+
+final Env env = Env.fromJson(_env);
