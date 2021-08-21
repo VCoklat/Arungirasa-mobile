fc60446dbddd25283973e28bdf05268879d8f05c
Leon Colt <lionmanbloodsucker@yahoo.com>
2021-08-21 14:42:18 +0700

    add make orde button simplify cart card widget

--- lib/view/cart/cart_page.dart
+++ lib/view/cart/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:arungi_rasa/common/config.dart';
 import 'package:arungi_rasa/common/helper.dart';
 import 'package:arungi_rasa/generated/l10n.dart';
 import 'package:arungi_rasa/model/cart.dart';
+import 'package:arungi_rasa/routes/routes.dart';
 import 'package:arungi_rasa/service/cart_service.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -28,14 +29,39 @@ class CartPage extends GetView<_CartPageController> {
           ],
           body: new Padding(
             padding: const EdgeInsets.all(10.0),
-            child: new AnimatedList(
-              key: controller.listState,
-              initialItemCount: controller.itemList.length,
-              itemBuilder: (_, final int index, final animation) =>
-                  new _CartCard(
-                cart: controller.itemList[index],
-                animation: animation,
-              ),
+            child: new Column(
+              children: [
+                new Expanded(
+                  child: new AnimatedList(
+                    key: controller.listState,
+                    initialItemCount: controller.itemList.length,
+                    itemBuilder: (_, final int index, final animation) =>
+                        new _AnimatedCartCard(
+                      cart: controller.itemList[index],
+                      animation: animation,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                new ElevatedButton(
+                  child: new Text(S.current.ordeButtonr),
+                  style: new ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        new RoundedRectangleBorder(
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(30))),
+                      ),
+                      padding:
+                          MaterialStateProperty.all(const EdgeInsets.all(15.0)),
+                      backgroundColor:
+                          MaterialStateProperty.all(Get.theme.accentColor),
+                      textStyle: MaterialStateProperty.all(const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.0,
+                      ))),
+                  onPressed: () => Get.offNamed(Routes.makeOrder),
+                ),
+              ],
             ),
           ),
         ),
@@ -44,89 +70,81 @@ class CartPage extends GetView<_CartPageController> {
 
 class _CartCard extends StatelessWidget {
   final Rx<Cart> cart;
-  final Animation<double> animation;
   const _CartCard({
     Key? key,
     required this.cart,
-    required this.animation,
   }) : super(key: key);
   @override
-  Widget build(BuildContext context) => new SlideTransition(
-        position: Tween<Offset>(
-          begin: const Offset(-1, 0),
-          end: const Offset(0, 0),
-        ).animate(animation),
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            new LayoutBuilder(
-              builder: (_, constraints) => new SizedBox(
-                height: constraints.maxWidth * 0.4,
-                child: new Row(
-                  children: [
-                    _getImage(constraints.maxWidth * 0.4),
-                    const SizedBox(
-                      width: 5.0,
-                    ),
-                    new Expanded(
-                      child: new Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          _title,
-                          const SizedBox(height: 5.0),
-                          _description,
-                          const SizedBox(height: 5.0),
-                          _totalText,
-                          const SizedBox(height: 5.0),
-                          new Row(
-                            children: [
-                              new IconButton(
-                                icon: const Icon(Icons.delete_sharp),
-                                iconSize: 28,
-                                onPressed: () => CartService.instance
-                                    .removeCart(cart.value.menu, 0),
-                              ),
-                              const SizedBox(width: 10.0),
-                              new IconButton(
-                                icon: const Icon(Icons.remove_sharp),
-                                iconSize: 28,
-                                onPressed: () => CartService.instance
-                                    .subtractCart(cart.value.menu),
-                              ),
-                              const SizedBox(width: 2.5),
-                              new ObxValue<Rx<Cart>>(
-                                (obs) => new Text(
-                                  obs.value.qty.toString(),
-                                  style: const TextStyle(
-                                    decoration: TextDecoration.underline,
-                                  ),
+  Widget build(BuildContext context) => new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          new LayoutBuilder(
+            builder: (_, constraints) => new SizedBox(
+              height: constraints.maxWidth * 0.4,
+              child: new Row(
+                children: [
+                  _getImage(constraints.maxWidth * 0.4),
+                  const SizedBox(
+                    width: 5.0,
+                  ),
+                  new Expanded(
+                    child: new Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _title,
+                        const SizedBox(height: 5.0),
+                        _description,
+                        const SizedBox(height: 5.0),
+                        _totalText,
+                        const SizedBox(height: 5.0),
+                        new Row(
+                          children: [
+                            new IconButton(
+                              icon: const Icon(Icons.delete_sharp),
+                              iconSize: 28,
+                              onPressed: () => CartService.instance
+                                  .removeCart(cart.value.menu, 0),
+                            ),
+                            const SizedBox(width: 10.0),
+                            new IconButton(
+                              icon: const Icon(Icons.remove_sharp),
+                              iconSize: 28,
+                              onPressed: () => CartService.instance
+                                  .subtractCart(cart.value.menu),
+                            ),
+                            const SizedBox(width: 2.5),
+                            new ObxValue<Rx<Cart>>(
+                              (obs) => new Text(
+                                obs.value.qty.toString(),
+                                style: const TextStyle(
+                                  decoration: TextDecoration.underline,
                                 ),
-                                cart,
                               ),
-                              const SizedBox(width: 2.5),
-                              new IconButton(
-                                icon: const Icon(Icons.add_sharp),
-                                iconSize: 28,
-                                onPressed: () => CartService.instance
-                                    .addCart(cart.value.menu),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
+                              cart,
+                            ),
+                            const SizedBox(width: 2.5),
+                            new IconButton(
+                              icon: const Icon(Icons.add_sharp),
+                              iconSize: 28,
+                              onPressed: () =>
+                                  CartService.instance.addCart(cart.value.menu),
+                            ),
+                          ],
+                        )
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(
-              height: 5.0,
-            ),
-            const Divider(),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 5.0,
+          ),
+          const Divider(),
+        ],
       );
 
   Widget get _totalText => new Expanded(
@@ -184,6 +202,24 @@ class _CartCard extends StatelessWidget {
       );
 }
 
+class _AnimatedCartCard extends StatelessWidget {
+  final Rx<Cart> cart;
+  final Animation<double> animation;
+  const _AnimatedCartCard({
+    Key? key,
+    required this.cart,
+    required this.animation,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) => new SlideTransition(
+        position: Tween<Offset>(
+          begin: const Offset(-1, 0),
+          end: const Offset(0, 0),
+        ).animate(animation),
+        child: new _CartCard(cart: cart),
+      );
+}
+
 class _CartPageController extends GetxController {
   final listState = new GlobalKey<AnimatedListState>();
   final itemList = <Rx<Cart>>[];
@@ -216,7 +252,7 @@ class _CartPageController extends GetxController {
     itemList.removeAt(index);
     listState.currentState!.removeItem(
       index,
-      (context, animation) => new _CartCard(
+      (context, animation) => new _AnimatedCartCard(
         cart: item,
         animation: animation,
       ),
