232e316e2129f26f1d37d7cb6de6a187dbb5ab38
Leon Colt <lionmanbloodsucker@yahoo.com>
2021-08-30 15:21:01 +0700

    refactor create to json function

--- lib/model/interest.dart
+++ lib/model/interest.dart
@@ -2,12 +2,14 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'interest.g.dart';
 
-@JsonSerializable( createToJson: false )
+@JsonSerializable()
 class Interest {
-  @JsonKey( required: true, disallowNullValue: true )
+  @JsonKey(required: true, disallowNullValue: true)
   final int id;
-  @JsonKey( required: true, disallowNullValue: true )
+  @JsonKey(required: true, disallowNullValue: true)
   final String name;
   Interest(this.id, this.name);
-  factory Interest.fromJson( final Map<String, dynamic> json ) => _$InterestFromJson( json );
+  factory Interest.fromJson(final Map<String, dynamic> json) =>
+      _$InterestFromJson(json);
+  Map<String, dynamic> toJson() => _$InterestToJson(this);
 }
