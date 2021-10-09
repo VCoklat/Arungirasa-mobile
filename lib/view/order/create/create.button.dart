a930d7704a93601c8471c1f3f9156591846907da
Leon Colt <lionmanbloodsucker@yahoo.com>
2021-10-09 14:47:27 +0700

    reduce height

--- lib/view/order/create/create.button.dart
+++ lib/view/order/create/create.button.dart
@@ -3,28 +3,34 @@ part of 'create_order_page.dart';
 class _CreateButton extends GetView<_CreateOrderPageController> {
   const _CreateButton();
   @override
-  Widget build(BuildContext context) => LoadingButton(
-        child: Text(S.current.processTransaction),
-        successChild: const Icon(
-          Icons.check_sharp,
-          size: 35,
-          color: Colors.white,
-        ),
-        errorChild: const Icon(
-          Icons.close_sharp,
-          size: 35,
-          color: Colors.white,
-        ),
-        style: ButtonStyle(
+  Widget build(BuildContext context) => SizedBox(
+        height: 40,
+        child: LoadingButton(
+          child: Text(S.current.processTransaction),
+          successChild: const Icon(
+            Icons.check_sharp,
+            size: 35,
+            color: Colors.white,
+          ),
+          errorChild: const Icon(
+            Icons.close_sharp,
+            size: 35,
+            color: Colors.white,
+          ),
+          style: ButtonStyle(
             shape: MaterialStateProperty.all(
               const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(30))),
             ),
             backgroundColor: MaterialStateProperty.all(Get.theme.accentColor),
-            textStyle: MaterialStateProperty.all(const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24.0,
-            ))),
-        onPressed: controller.createOrder,
+            textStyle: MaterialStateProperty.all(
+              const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+              ),
+            ),
+          ),
+          onPressed: controller.createOrder,
+        ),
       );
 }
