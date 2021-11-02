497bc834c12188d13bd6aac3967c9e4358ff6596
Leon Colt <lionmanbloodsucker@yahoo.com>
2021-11-02 23:00:38 +0700

    add some constant configurations

--- lib/common/config.dart
+++ lib/common/config.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 const kIgnoreBadCertificate = true;
 
@@ -24,3 +25,27 @@ const kDefaultLatitude = -6.1754137242424445;
 const kDefaultLongitude = 106.82715279991743;
 
 const kItemPerPage = 10;
+
+const kPagePaddingHorizontoal = 20.0;
+const kPagePaddingVertical = 10.0;
+
+const kSecondaryButtonHeight = 30.0;
+
+const kPagePadding = EdgeInsets.symmetric(
+    horizontal: kPagePaddingHorizontoal, vertical: kPagePaddingVertical);
+
+double get secondaryButtonSize =>
+    (Get.width - (kPagePaddingHorizontoal * 2)) / 3;
+
+final seondaryButtonStyle = ButtonStyle(
+    padding:
+        MaterialStateProperty.all(const EdgeInsets.symmetric(horizontal: 5.0)),
+    backgroundColor: MaterialStateProperty.all(Colors.white),
+    shape: MaterialStateProperty.all(const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(15.0)))),
+    side: MaterialStateProperty.all(BorderSide(color: Get.theme.primaryColor)),
+    foregroundColor: MaterialStateProperty.all(Get.theme.primaryColor),
+    textStyle: MaterialStateProperty.all(const TextStyle(
+      fontSize: 12.0,
+      fontWeight: FontWeight.bold,
+    )));
